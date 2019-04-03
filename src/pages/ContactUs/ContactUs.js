adf0f37adc0c1c44e32267a20eaef578f5bb92ee
MillennialMechanics <47344403+MillennialMechanics@users.noreply.github.com>
2019-04-02 18:41:34 -0700

    Update ContactUs.js

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -7,8 +7,12 @@ const ContactUs = () => {
     <div className="contact-us">
       <h3>Contact Us</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque a illo
-        dolorem vero, ea molestiae non quasi facilis nulla beatae.
+        Here at Millennial Mechanics we are always open to hear suggestions 
+        from those who 	get out there and repair their cars, whether it is 
+        for fun or to save money Millennial Mechanics is the place to go 
+        for all of your repair needs. We would love to get some feedback or 
+        be able to answer any questions you may		have by reaching out to us 
+        via email at Millennialmechanics@gmail.com
       </p>
     </div>
   );
