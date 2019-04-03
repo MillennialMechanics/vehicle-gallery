cf97f84531182a5259af8322b440bf7647067cf4
MillennialMechanics <47344403+MillennialMechanics@users.noreply.github.com>
2019-04-02 18:36:54 -0700

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,9 +7,14 @@ const About = () => {
     <div className="about">
       <h3>About us</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est repellat
-        necessitatibus consectetur ipsum accusamus nam, magni laborum eligendi
-        culpa at?
+        We realized that fixing a car was as simple as putting legos together. 
+        So we made a site that shows you just how easy it is; saving you money, 
+        time, and the hassle of dealing with overpriced mechanics. Our mission 
+        is to bring car repairs to the home as efficiently as possible without 
+        sacrificing the quality of mechanical repairs. Find the videos and parts 
+        you need for the repair, or just check how much the mechanic will charge 
+        to do it. Look for your car with the interactive vehicle maps to find any 
+        repairs you need!
       </p>
     </div>
   );
